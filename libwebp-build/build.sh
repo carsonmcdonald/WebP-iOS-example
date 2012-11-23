648090d95fbf3880f4b2a07c6f4f5c2ab0df524b
lqs <lqs@jiepang.com>
2012-11-23 12:57:04 +0800

    support for newer version of iOS SDK and libwebp.

--- libwebp-build/build.sh
+++ libwebp-build/build.sh
@@ -8,13 +8,14 @@
 # with the name WebP.framework
 #
 
-SDK=4.3
-PLATFORMS="iPhoneSimulator iPhoneOS-V6 iPhoneOS-V7"
-DEVELOPER=/Developer
+SDK=6.0
+PLATFORMS="iPhoneSimulator iPhoneOS-V7 iPhoneOS-V7s"
+DEVELOPER=/Applications/Xcode.app/Contents/Developer
 TOPDIR=`pwd`
 BUILDDIR="$TOPDIR/tmp"
 FINALDIR="$TOPDIR/WebP.framework"
 LIBLIST=''
+WEBPSRC=libwebp-0.2.1
 
 mkdir -p $BUILDDIR
 mkdir -p $FINALDIR
@@ -22,7 +23,11 @@ mkdir $FINALDIR/Headers/
 
 for PLATFORM in ${PLATFORMS}
 do
-  if [ "${PLATFORM}" == "iPhoneOS-V7" ]
+  if [ "${PLATFORM}" == "iPhoneOS-V7s" ]
+  then
+    PLATFORM="iPhoneOS"
+    ARCH="armv7s"
+  elif [ "${PLATFORM}" == "iPhoneOS-V7" ]
   then
     PLATFORM="iPhoneOS"
     ARCH="armv7"
@@ -42,7 +47,7 @@ do
   export SDKROOT="${DEVROOT}/SDKs/${PLATFORM}${SDK}.sdk"
   export CC=${DEVROOT}/usr/bin/gcc
   export LD=${DEVROOT}/usr/bin/ld
-  export CPP=${DEVROOT}/usr/bin/cpp
+  export CPP=${DEVELOPER}/usr/bin/cpp
   export CXX=${DEVROOT}/usr/bin/g++
   export AR=${DEVROOT}/usr/bin/ar
   export AS=${DEVROOT}/usr/bin/as
@@ -53,15 +58,17 @@ do
   export CFLAGS="-arch ${ARCH} -pipe -no-cpp-precomp -isysroot ${SDKROOT} -I${ROOTDIR}/include"
   export CXXFLAGS="-arch ${ARCH} -pipe -no-cpp-precomp -isysroot ${SDKROOT} -I${ROOTDIR}/include"
 
-  rm -rf libwebp-0.1.2
-  tar xzf libwebp-0.1.2.tar.gz
-  cd libwebp-0.1.2
+  rm -rf $WEBPSRC
+  tar xzf $WEBPSRC.tar.gz
+  cd $WEBPSRC
 
   sh autogen.sh
 
   ./configure --host=${ARCH}-apple-darwin --prefix=${ROOTDIR} --disable-shared --enable-static
   make
+  cd src
   make install
+  cd ..
 
   LIBLIST="${LIBLIST} ${ROOTDIR}/lib/libwebp.a"
   cp -Rp ${ROOTDIR}/include/webp/* $FINALDIR/Headers/
@@ -71,5 +78,5 @@ done
 
 ${DEVROOT}/usr/bin/lipo -create $LIBLIST -output $FINALDIR/WebP
 
-rm -rf libwebp-0.1.2
+rm -rf $WEBPSRC
 rm -rf ${BUILDDIR}
