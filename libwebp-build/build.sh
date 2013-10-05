1198fcfb7c84ea4ab51655b936e1cb6fb464db72
Carson McDonald <carson@ioncannon.net>
2013-10-05 12:49:47 -0400

    Updated for xcode 5

--- libwebp-build/build.sh
+++ libwebp-build/build.sh
@@ -8,13 +8,14 @@
 # with the name WebP.framework
 #
 
-SDK=6.1
+SDK=7.0
 PLATFORMS="iPhoneSimulator iPhoneOS-V7 iPhoneOS-V7s"
 DEVELOPER=`xcode-select -print-path`
 TOPDIR=`pwd`
 BUILDDIR="$TOPDIR/tmp"
 FINALDIR="$TOPDIR/WebP.framework"
 LIBLIST=''
+DEVROOT="${DEVELOPER}/Toolchains/XcodeDefault.xctoolchain"
 
 mkdir -p $BUILDDIR
 mkdir -p $FINALDIR
@@ -24,40 +25,40 @@ for PLATFORM in ${PLATFORMS}
 do
   if [ "${PLATFORM}" == "iPhoneOS-V7" ]
   then
-    PLATFORM="iPhoneOS"
+    SDKPATH="${DEVELOPER}/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS7.0.sdk/"
     ARCH="armv7"
   elif [ "${PLATFORM}" == "iPhoneOS-V7s" ]
   then
-    PLATFORM="iPhoneOS"
+    SDKPATH="${DEVELOPER}/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS7.0.sdk/"
     ARCH="armv7s"
   else
+    SDKPATH="${DEVELOPER}/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator7.0.sdk/"
     ARCH="i386"
   fi
 
-  ROOTDIR="${BUILDDIR}/${PLATFORM}-${SDK}-${ARCH}"
-  rm -rf "${ROOTDIR}"
-  mkdir -p "${ROOTDIR}"
-
-  export DEVROOT="${DEVELOPER}/Platforms/${PLATFORM}.platform/Developer"
-  export SDKROOT="${DEVROOT}/SDKs/${PLATFORM}${SDK}.sdk"
-  export CC=${DEVROOT}/usr/bin/gcc
+  export CC=${DEVROOT}/usr/bin/cc
   export LD=${DEVROOT}/usr/bin/ld
-  export CPP=${DEVROOT}/usr/bin/llvm-cpp-4.2
+  export CPP=${DEVROOT}/usr/bin/cpp
   export CXX=${DEVROOT}/usr/bin/g++
   export AR=${DEVROOT}/usr/bin/ar
   export AS=${DEVROOT}/usr/bin/as
   export NM=${DEVROOT}/usr/bin/nm
-  export CXXCPP=$DEVROOT/usr/bin/llvm-cpp-4.2
-  export RANLIB=$DEVROOT/usr/bin/ranlib
-  export LDFLAGS="-arch ${ARCH} -pipe -no-cpp-precomp -isysroot ${SDKROOT} -L${ROOTDIR}/lib"
-  export CFLAGS="-arch ${ARCH} -pipe -no-cpp-precomp -isysroot ${SDKROOT} -I${ROOTDIR}/include"
-  export CXXFLAGS="-arch ${ARCH} -pipe -no-cpp-precomp -isysroot ${SDKROOT} -I${ROOTDIR}/include"
+  export CXXCPP=${DEVROOT}/usr/bin/cpp
+  export RANLIB=${DEVROOT}/usr/bin/ranlib
 
   rm -rf libwebp-0.3.1
   tar xzf libwebp-0.3.1.tar.gz
   cd libwebp-0.3.1
 
   sh autogen.sh
+
+  ROOTDIR="/tmp/install.$$.${ARCH}"
+  rm -rf "${ROOTDIR}"
+  mkdir -p "${ROOTDIR}"
+
+  export LDFLAGS="-arch ${ARCH} -miphoneos-version-min=5.0 -pipe -no-cpp-precomp -isysroot ${SDKPATH}"
+  export CFLAGS="-arch ${ARCH} -miphoneos-version-min=5.0 -pipe -no-cpp-precomp -isysroot ${SDKPATH}"
+  export CXXFLAGS="-arch ${ARCH} -miphoneos-version-min=5.0 -pipe -no-cpp-precomp -isysroot ${SDKPATH}"
 
   ./configure --host=${ARCH}-apple-darwin --prefix=${ROOTDIR} --disable-shared --enable-static
   make
