7495c95bb766cb4e602166979626bcac1144f57b
Carson McDonald <carson@ioncannon.net>
2013-10-09 18:43:31 -0400

    Add some run time output

--- TestWebP/TestWebP/ViewController.m
+++ TestWebP/TestWebP/ViewController.m
@@ -37,23 +37,39 @@ static void free_image_data(void *info, const void *data, size_t size)
 
 - (void)displayImage:(NSString *)filePath
 {
+    NSLog(@"* * * * * * * * * * *");
+    
+    NSDate *startTime = [NSDate date];
+    
     // Find the path of the selected WebP image in the bundle and read it into memory
     NSData *myData = [NSData dataWithContentsOfFile:filePath];
+    
+    NSLog(@"Time to read data: %0.2fms", [startTime timeIntervalSinceNow] * -1000.0);
     
     // Get the current version of the WebP decoder
     int rc = WebPGetDecoderVersion();
     
     NSLog(@"Version: %d", rc);
     
+    startTime = [NSDate date];
+    
     // Get the width and height of the selected WebP image
     int width = 0;
     int height = 0;
     WebPGetInfo([myData bytes], [myData length], &width, &height);
     
+    NSLog(@"Time to read info: %0.2fms", [startTime timeIntervalSinceNow] * -1000.0);
+    
     NSLog(@"Image Width: %d Image Height: %d", width, height);
+    
+    startTime = [NSDate date];
     
     // Decode the WebP image data into a RGBA value array
     uint8_t *data = WebPDecodeRGBA([myData bytes], [myData length], &width, &height);
+    
+    NSLog(@"Time to decode WebP data: %0.2fms", [startTime timeIntervalSinceNow] * -1000.0);
+    
+    startTime = [NSDate date];
     
     // Construct a UIImage from the decoded RGBA value array
     CGDataProviderRef provider = CGDataProviderCreateWithData(NULL, data, width*height*4, free_image_data);
@@ -62,6 +78,8 @@ static void free_image_data(void *info, const void *data, size_t size)
     CGColorRenderingIntent renderingIntent = kCGRenderingIntentDefault;
     CGImageRef imageRef = CGImageCreate(width, height, 8, 32, 4*width, colorSpaceRef, bitmapInfo, provider, NULL, NO, renderingIntent);
     UIImage *newImage = [UIImage imageWithCGImage:imageRef];
+    
+    NSLog(@"Time to contruct UIImage from RGBA: %0.2fms", [startTime timeIntervalSinceNow] * -1000.0);
     
     // Set the image into the image view and make image view and the scroll view to the correct size
     self.testImageView.frame = CGRectMake(0, 0, width, height);
